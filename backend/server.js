123b2a0e1d0fb0c918af6ef18f9ee07c3dd3cd0d
mishthimahajan <mishthimahajan0@gmail.com>
2026-05-24 01:40:09 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,23 @@ const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+
+cors({
+
+origin:[
+
+"http://localhost:5173",
+
+"https://manufacturing-bda-crm.vercel.app/"
+
+],
+
+credentials:true
+
+})
+
+);
 
 app.use(express.json());
 
